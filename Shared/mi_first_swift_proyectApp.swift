cdb98363b140b10fc99ac90e3ce6e7a1b3823cc3
hebermudezg <hebermudezg@unal.edu.co>
2022-02-13 16:02:46 -0500

    Initial Commit

---
+++ Shared/mi_first_swift_proyectApp.swift
@@ -0,0 +1,17 @@
+//
+//  mi_first_swift_proyectApp.swift
+//  Shared
+//
+//  Created by Heber E. Bermudez on 2/13/22.
+//
+
+import SwiftUI
+
+@main
+struct mi_first_swift_proyectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
